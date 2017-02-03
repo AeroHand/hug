2cc68eed5e3f06ffcb1c7ec0015474582f6f391f
JayNarv <jay@narvalous.com>
2017-02-03 15:48:09 -0800

    singleton, UI

---
+++ Assets/Scripts/Structure/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : Singleton<GameManager>
+{
+
+    protected GameManager() { } //good coding practices right here fucker
+
+    public void replay()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void lose()
+    {
+        Application.LoadLevel(2);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/UImanager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UImanager : MonoBehaviour {
+
+    public GameObject HomeInterface;
+    public GameObject ChatInterface;
+    public GameObject FriendsInterface;
+    public GameObject ProfileInterface;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void onHomeBtnClick() {
+        HomeInterface.SetActive(true);
+        ChatInterface.SetActive(false);
+        FriendsInterface.SetActive(false);
+        ProfileInterface.SetActive(false);
+    }
+
+    public void onExploreBtnClick()
+    {
+        HomeInterface.SetActive(false);
+        ChatInterface.SetActive(false);
+        FriendsInterface.SetActive(false);
+        ProfileInterface.SetActive(false);
+    }
+
+    public void onChatBtnClick()
+    {
+        HomeInterface.SetActive(false);
+        ChatInterface.SetActive(true);
+        FriendsInterface.SetActive(false);
+        ProfileInterface.SetActive(false);
+    }
+
+    public void onFriendsBtnClick()
+    {
+        HomeInterface.SetActive(false);
+        ChatInterface.SetActive(false);
+        FriendsInterface.SetActive(true);
+        ProfileInterface.SetActive(false);
+    }
+
+    public void onProfileBtnClick()
+    {
+        HomeInterface.SetActive(false);
+        ChatInterface.SetActive(false);
+        FriendsInterface.SetActive(false);
+        ProfileInterface.SetActive(true);
+    }
+}
